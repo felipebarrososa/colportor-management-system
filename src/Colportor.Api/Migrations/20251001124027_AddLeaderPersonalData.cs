bfc48f1cb8faa6283c81231cae0fa483c3a944bb
felipebarrosoa <fsbarroso0890@gmail.com>
2025-10-01 09:40:38 -0300

    Add migration for leader personal data fields

---
+++ src/Colportor.Api/Migrations/20251001124027_AddLeaderPersonalData.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Colportor.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLeaderPersonalData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Iso2 = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NotificationLogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ColportorId = table.Column<int>(type: "integer", nullable: false),
+                    Type = table.Column<string>(type: "text", nullable: false),
+                    SentAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NotificationLogs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Regions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    CountryId = table.Column<int>(type: "integer", nullable: false),
+                    CountryId1 = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Regions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Regions_Countries_CountryId",
+                        column: x => x.CountryId,
+                        principalTable: "Countries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Regions_Countries_CountryId1",
+                        column: x => x.CountryId1,
+                        principalTable: "Countries",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Colportors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FullName = table.Column<string>(type: "text", nullable: false),
+                    CPF = table.Column<string>(type: "text", nullable: false),
+                    CountryId = table.Column<int>(type: "integer", nullable: true),
+                    RegionId = table.Column<int>(type: "integer", nullable: true),
+                    City = table.Column<string>(type: "text", nullable: true),
+                    PhotoUrl = table.Column<string>(type: "text", nullable: true),
+                    LastVisitDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    LeaderId = table.Column<int>(type: "integer", nullable: true),
+                    LeaderId1 = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Colportors", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Colportors_Countries_CountryId",
+                        column: x => x.CountryId,
+                        principalTable: "Countries",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Colportors_Regions_RegionId",
+                        column: x => x.RegionId,
+                        principalTable: "Regions",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    PasswordHash = table.Column<string>(type: "text", nullable: false),
+                    Role = table.Column<string>(type: "text", nullable: false),
+                    FullName = table.Column<string>(type: "text", nullable: true),
+                    CPF = table.Column<string>(type: "text", nullable: true),
+                    City = table.Column<string>(type: "text", nullable: true),
+                    ColportorId = table.Column<int>(type: "integer", nullable: true),
+                    RegionId = table.Column<int>(type: "integer", nullable: true),
+                    RegionId1 = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_Colportors_ColportorId",
+                        column: x => x.ColportorId,
+                        principalTable: "Colportors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_Users_Regions_RegionId",
+                        column: x => x.RegionId,
+                        principalTable: "Regions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_Users_Regions_RegionId1",
+                        column: x => x.RegionId1,
+                        principalTable: "Regions",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Visits",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ColportorId = table.Column<int>(type: "integer", nullable: false),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Visits", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Visits_Colportors_ColportorId",
+                        column: x => x.ColportorId,
+                        principalTable: "Colportors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PacEnrollments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    LeaderId = table.Column<int>(type: "integer", nullable: false),
+                    ColportorId = table.Column<int>(type: "integer", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Status = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PacEnrollments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PacEnrollments_Colportors_ColportorId",
+                        column: x => x.ColportorId,
+                        principalTable: "Colportors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PacEnrollments_Users_LeaderId",
+                        column: x => x.LeaderId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Colportors_CPF",
+                table: "Colportors",
+                column: "CPF",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Colportors_CountryId",
+                table: "Colportors",
+                column: "CountryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Colportors_LeaderId1",
+                table: "Colportors",
+                column: "LeaderId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Colportors_RegionId",
+                table: "Colportors",
+                column: "RegionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PacEnrollments_ColportorId_StartDate_EndDate",
+                table: "PacEnrollments",
+                columns: new[] { "ColportorId", "StartDate", "EndDate" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PacEnrollments_LeaderId",
+                table: "PacEnrollments",
+                column: "LeaderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Regions_CountryId1",
+                table: "Regions",
+                column: "CountryId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Regions_CountryId_Name",
+                table: "Regions",
+                columns: new[] { "CountryId", "Name" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_ColportorId",
+                table: "Users",
+                column: "ColportorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_RegionId",
+                table: "Users",
+                column: "RegionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_RegionId1",
+                table: "Users",
+                column: "RegionId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Visits_ColportorId",
+                table: "Visits",
+                column: "ColportorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Colportors_Users_LeaderId1",
+                table: "Colportors",
+                column: "LeaderId1",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Colportors_Countries_CountryId",
+                table: "Colportors");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Regions_Countries_CountryId",
+                table: "Regions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Regions_Countries_CountryId1",
+                table: "Regions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Colportors_Regions_RegionId",
+                table: "Colportors");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Regions_RegionId",
+                table: "Users");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Regions_RegionId1",
+                table: "Users");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Colportors_Users_LeaderId1",
+                table: "Colportors");
+
+            migrationBuilder.DropTable(
+                name: "NotificationLogs");
+
+            migrationBuilder.DropTable(
+                name: "PacEnrollments");
+
+            migrationBuilder.DropTable(
+                name: "Visits");
+
+            migrationBuilder.DropTable(
+                name: "Countries");
+
+            migrationBuilder.DropTable(
+                name: "Regions");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Colportors");
+        }
+    }
+}
