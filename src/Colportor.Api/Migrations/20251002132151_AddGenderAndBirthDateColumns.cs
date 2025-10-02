2af690999a60953e9a4b58ff3f4a4d93b851243a
felipebarrosoa <fsbarroso0890@gmail.com>
2025-10-02 10:22:28 -0300

    Add Gender and BirthDate columns migration

---
+++ src/Colportor.Api/Migrations/20251002132151_AddGenderAndBirthDateColumns.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Colportor.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGenderAndBirthDateColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                table: "Colportors",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BirthDate",
+                table: "Colportors",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BirthDate",
+                table: "Colportors");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "Colportors");
+        }
+    }
+}
